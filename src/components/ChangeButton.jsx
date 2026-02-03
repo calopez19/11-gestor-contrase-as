8a186b3fb2072e6a85a5b9e2e5451d3d87b1ba2e
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2026-02-03 19:32:53 -0300

    add card's styles

---
+++ src/components/ChangeButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/changeButton.css";
+
+const ChangeButton = () => {
+  return (
+    <button className="button" type="button">
+      <span className="button__text">change</span>
+      <span className="button__icon">
+      </span>
+    </button>
+  );
+};
+
+export default ChangeButton;
--- src/components/FlippingCard.jsx
+++ src/components/FlippingCard.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { motion } from "motion/react";
 import gmail from "../../public/image/gmail.png";
 import "../styles/card.css";
+import { SettingsOutline } from "../assets/settings.jsx";
+import DeleteButton from "./DeleteButton.jsx";
+import ChangeButton from "./ChangeButton.jsx";
 
 export function FlippingCard() {
   const [isFlipping, setIsFlipping] = useState(false);
@@ -20,11 +23,42 @@ export function FlippingCard() {
           className="flip-card-inner"
           initial={false}
           animate={{ rotateY: isFlipping ? 180 : 360 }}
-          transition={{duration: 0.6, animationDirection:'normal'}}
-          onAnimationComplete={() =>  setIsAnimated(false) }
+          transition={{ duration: 0.6, animationDirection: "normal" }}
+          onAnimationComplete={() => setIsAnimated(false)}
         >
-          <div className="flip-card-front"></div>
-          <div className="flip-card-back"></div>
+          <div className="card flip-card-front">
+            <img src={gmail} alt="" />
+          </div>
+          <div className="card flip-card-back">
+            <div
+              style={{
+                height: "50%",
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "0 20px",
+                boxSizing: "border-box",
+              }}
+            >
+              <div>
+                <img
+                  src={gmail}
+                  alt="gmail icon"
+                  style={{ width: "50px", height: "50px" }}
+                />
+              </div>
+              <div>
+                <p>usuario</p>
+                <p>*******</p>
+              </div>
+              <SettingsOutline />
+            </div>
+            <div>
+              <DeleteButton/>
+              <ChangeButton/>
+            </div>
+          </div>
         </motion.div>
       </div>
     </>
